f172675a2d358ed28416e22a8dd6bfeff26e92ac
Djidjelly Siclait <djidjellypas92@gmail.com>
2019-11-28 14:21:23 +0100

    Add create table script for product category entity

---
+++ Database Scripts/Create Table Product Categories.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Tbl_Product_Categories
+(
+	fld_category_id varchar(5) NOT NULL, -- Automatically generated identification code for product categories
+	fld_category_description varchar(50), -- name of product brand for detailed categorization and grouping purposes 
+
+	PRIMARY KEY (fld_category_id)
+)
