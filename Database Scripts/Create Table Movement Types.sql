8ddce3f2cc1cf7e2c52e0825841af1d4911587c2
Djidjelly Siclait <djidjellypas92@gmail.com>
2019-11-29 09:49:20 +0100

    Add create table script for product movement type

---
+++ Database Scripts/Create Table Movement Types.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Tbl_Movement_Types
+(
+	fld_type_id varchar(4), -- Automatically generated identification code for movement type
+	fld_type_description varchar(100), -- description  of movement type
+
+	PRIMARY KEY (fld_type_id)
+)
