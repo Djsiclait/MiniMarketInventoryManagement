bcfcd8ec9ede9b5127a522d44d059ece624357fd
Djidjelly Siclait <djidjellypas92@gmail.com>
2019-11-29 15:26:54 +0100

    Add create table script for product types

---
+++ Database Scripts/Create Table Product Type.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Tbl_Product_Type
+(
+	fld_type_id varchar(5) NOT NULL, -- Automatically generated identification code for product type
+	fld_type_description varchar(50), -- description of product type
+	
+	PRIMARY KEY (fld_type_id) 
+)
