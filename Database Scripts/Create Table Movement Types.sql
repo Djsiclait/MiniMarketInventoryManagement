58eb353bfe782baa18f86dda9413479836ea57aa
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-01-22 13:06:15 +0100

    Delete unnecessary entity table scripts

--- Database Scripts/Create Table Movement Types.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Tbl_Movement_Types
-(
-	fld_type_id varchar(4) NOT NULL, -- Automatically generated identification code for movement type
-	fld_type_description varchar(100) Default '', -- description of movement type
-
-	PRIMARY KEY (fld_type_id)
-)
--- Database Scripts/Create Table Roles.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Tbl_Roles
-(
-	fld_role_id varchar(3),
-	fld_role_description varchar(30) Unique,
-
-	Primary Key (fld_Role_Id)
-)
