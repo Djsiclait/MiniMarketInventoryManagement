dbd7c103e40f2ca764b6f1e50c97d6071cddbdd0
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-01-22 11:16:02 +0100

    Add create table script for supplier entity

---
+++ Database Scripts/Create Table Suppliers.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Tbl_Suppliers
+(
+	fld_supplier_id varchar(4),
+	fld_supplier_name varchar(100) Unique,
+	fld_supplier_phone_number varchar(100) Unique,
+	fld_supplier_email varchar(200) Unique,
+	
+	Primary Key (fld_supplier_id)
+)
