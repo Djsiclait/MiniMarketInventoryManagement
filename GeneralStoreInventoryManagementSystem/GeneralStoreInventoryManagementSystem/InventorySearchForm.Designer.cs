2fa24b9504cf8abda3098bd3c9bfda5a0ec4c88f
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-01-23 11:16:14 +0100

    Design inventory browser form

--- GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/InventorySearchForm.Designer.cs
+++ GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/InventorySearchForm.Designer.cs
@@ -28,35 +28,261 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.InventorySearchButton = new System.Windows.Forms.Button();
+            this.inventorySearchBox = new System.Windows.Forms.TextBox();
+            this.browserFormTitle = new System.Windows.Forms.Label();
+            this.productList = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.salesMenuOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewSalesMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.makeSaleMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.productsMenuOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.browseProductsMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.registerNewProductMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.restockProductsMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.adminMenuOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageUsersMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewUsersOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.registerNewUserOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.statisticsMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewGraphsIOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewReportsOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.logsMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewActivityLogsOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewErrorLogsOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.LogOutLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.productList)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // InventorySearchButton
             // 
-            this.button1.Location = new System.Drawing.Point(275, 179);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.InventorySearchButton.Location = new System.Drawing.Point(590, 41);
+            this.InventorySearchButton.Name = "InventorySearchButton";
+            this.InventorySearchButton.Size = new System.Drawing.Size(75, 23);
+            this.InventorySearchButton.TabIndex = 0;
+            this.InventorySearchButton.Text = "Search";
+            this.InventorySearchButton.UseVisualStyleBackColor = true;
+            // 
+            // inventorySearchBox
+            // 
+            this.inventorySearchBox.Location = new System.Drawing.Point(339, 41);
+            this.inventorySearchBox.Name = "inventorySearchBox";
+            this.inventorySearchBox.Size = new System.Drawing.Size(245, 20);
+            this.inventorySearchBox.TabIndex = 1;
+            // 
+            // browserFormTitle
+            // 
+            this.browserFormTitle.AutoSize = true;
+            this.browserFormTitle.Location = new System.Drawing.Point(66, 46);
+            this.browserFormTitle.Name = "browserFormTitle";
+            this.browserFormTitle.Size = new System.Drawing.Size(92, 13);
+            this.browserFormTitle.TabIndex = 2;
+            this.browserFormTitle.Text = "Inventory Browser";
+            // 
+            // productList
+            // 
+            this.productList.AllowUserToOrderColumns = true;
+            this.productList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.productList.Location = new System.Drawing.Point(12, 79);
+            this.productList.Name = "productList";
+            this.productList.Size = new System.Drawing.Size(653, 400);
+            this.productList.TabIndex = 3;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salesMenuOption,
+            this.productsMenuOption,
+            this.adminMenuOption});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(684, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // salesMenuOption
+            // 
+            this.salesMenuOption.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewSalesMenuSubOption,
+            this.makeSaleMenuSubOption});
+            this.salesMenuOption.Name = "salesMenuOption";
+            this.salesMenuOption.Size = new System.Drawing.Size(45, 20);
+            this.salesMenuOption.Text = "Sales";
+            // 
+            // viewSalesMenuSubOption
+            // 
+            this.viewSalesMenuSubOption.Name = "viewSalesMenuSubOption";
+            this.viewSalesMenuSubOption.Size = new System.Drawing.Size(152, 22);
+            this.viewSalesMenuSubOption.Text = "View Sales";
+            // 
+            // makeSaleMenuSubOption
+            // 
+            this.makeSaleMenuSubOption.Name = "makeSaleMenuSubOption";
+            this.makeSaleMenuSubOption.Size = new System.Drawing.Size(152, 22);
+            this.makeSaleMenuSubOption.Text = "Make Sale";
+            // 
+            // productsMenuOption
+            // 
+            this.productsMenuOption.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.browseProductsMenuSubOption,
+            this.registerNewProductMenuSubOption,
+            this.restockProductsMenuSubOption});
+            this.productsMenuOption.Name = "productsMenuOption";
+            this.productsMenuOption.Size = new System.Drawing.Size(66, 20);
+            this.productsMenuOption.Text = "Products";
+            // 
+            // browseProductsMenuSubOption
+            // 
+            this.browseProductsMenuSubOption.Name = "browseProductsMenuSubOption";
+            this.browseProductsMenuSubOption.Size = new System.Drawing.Size(188, 22);
+            this.browseProductsMenuSubOption.Text = "Browse Products";
+            // 
+            // registerNewProductMenuSubOption
+            // 
+            this.registerNewProductMenuSubOption.Name = "registerNewProductMenuSubOption";
+            this.registerNewProductMenuSubOption.Size = new System.Drawing.Size(188, 22);
+            this.registerNewProductMenuSubOption.Text = "Register New Product";
+            // 
+            // restockProductsMenuSubOption
+            // 
+            this.restockProductsMenuSubOption.Name = "restockProductsMenuSubOption";
+            this.restockProductsMenuSubOption.Size = new System.Drawing.Size(188, 22);
+            this.restockProductsMenuSubOption.Text = "Restock Products";
+            // 
+            // adminMenuOption
+            // 
+            this.adminMenuOption.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.manageUsersMenuSubOption,
+            this.statisticsMenuSubOption,
+            this.logsMenuSubOption});
+            this.adminMenuOption.Name = "adminMenuOption";
+            this.adminMenuOption.Size = new System.Drawing.Size(55, 20);
+            this.adminMenuOption.Text = "Admin";
+            // 
+            // manageUsersMenuSubOption
+            // 
+            this.manageUsersMenuSubOption.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewUsersOption,
+            this.registerNewUserOption});
+            this.manageUsersMenuSubOption.Name = "manageUsersMenuSubOption";
+            this.manageUsersMenuSubOption.Size = new System.Drawing.Size(152, 22);
+            this.manageUsersMenuSubOption.Text = "Manage Users";
+            // 
+            // viewUsersOption
+            // 
+            this.viewUsersOption.Name = "viewUsersOption";
+            this.viewUsersOption.Size = new System.Drawing.Size(169, 22);
+            this.viewUsersOption.Text = "View Users";
+            // 
+            // registerNewUserOption
+            // 
+            this.registerNewUserOption.Name = "registerNewUserOption";
+            this.registerNewUserOption.Size = new System.Drawing.Size(169, 22);
+            this.registerNewUserOption.Text = "Register New User";
+            // 
+            // statisticsMenuSubOption
+            // 
+            this.statisticsMenuSubOption.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewGraphsIOption,
+            this.viewReportsOption});
+            this.statisticsMenuSubOption.Name = "statisticsMenuSubOption";
+            this.statisticsMenuSubOption.Size = new System.Drawing.Size(152, 22);
+            this.statisticsMenuSubOption.Text = "Statistics";
+            // 
+            // viewGraphsIOption
+            // 
+            this.viewGraphsIOption.Name = "viewGraphsIOption";
+            this.viewGraphsIOption.Size = new System.Drawing.Size(152, 22);
+            this.viewGraphsIOption.Text = "View Graphs";
+            // 
+            // viewReportsOption
+            // 
+            this.viewReportsOption.Name = "viewReportsOption";
+            this.viewReportsOption.Size = new System.Drawing.Size(152, 22);
+            this.viewReportsOption.Text = "View Reports";
+            // 
+            // logsMenuSubOption
+            // 
+            this.logsMenuSubOption.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewActivityLogsOption,
+            this.viewErrorLogsOption});
+            this.logsMenuSubOption.Name = "logsMenuSubOption";
+            this.logsMenuSubOption.Size = new System.Drawing.Size(152, 22);
+            this.logsMenuSubOption.Text = "Logs";
+            // 
+            // viewActivityLogsOption
+            // 
+            this.viewActivityLogsOption.Name = "viewActivityLogsOption";
+            this.viewActivityLogsOption.Size = new System.Drawing.Size(170, 22);
+            this.viewActivityLogsOption.Text = "View Activity Logs";
+            // 
+            // viewErrorLogsOption
+            // 
+            this.viewErrorLogsOption.Name = "viewErrorLogsOption";
+            this.viewErrorLogsOption.Size = new System.Drawing.Size(170, 22);
+            this.viewErrorLogsOption.Text = "View Error Logs";
+            // 
+            // LogOutLabel
+            // 
+            this.LogOutLabel.AutoSize = true;
+            this.LogOutLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.LogOutLabel.Location = new System.Drawing.Point(637, 9);
+            this.LogOutLabel.Name = "LogOutLabel";
+            this.LogOutLabel.Size = new System.Drawing.Size(45, 13);
+            this.LogOutLabel.TabIndex = 5;
+            this.LogOutLabel.Text = "Log Out";
             // 
             // InventorySearchForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(684, 462);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(684, 491);
+            this.Controls.Add(this.LogOutLabel);
+            this.Controls.Add(this.productList);
+            this.Controls.Add(this.browserFormTitle);
+            this.Controls.Add(this.inventorySearchBox);
+            this.Controls.Add(this.InventorySearchButton);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
             this.MinimumSize = new System.Drawing.Size(700, 500);
             this.Name = "InventorySearchForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "InventorySearchForm";
+            this.Text = "Product Browser";
+            ((System.ComponentModel.ISupportInitialize)(this.productList)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button InventorySearchButton;
+        private System.Windows.Forms.TextBox inventorySearchBox;
+        private System.Windows.Forms.Label browserFormTitle;
+        private System.Windows.Forms.DataGridView productList;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem salesMenuOption;
+        private System.Windows.Forms.ToolStripMenuItem viewSalesMenuSubOption;
+        private System.Windows.Forms.ToolStripMenuItem makeSaleMenuSubOption;
+        private System.Windows.Forms.ToolStripMenuItem productsMenuOption;
+        private System.Windows.Forms.ToolStripMenuItem browseProductsMenuSubOption;
+        private System.Windows.Forms.ToolStripMenuItem registerNewProductMenuSubOption;
+        private System.Windows.Forms.ToolStripMenuItem restockProductsMenuSubOption;
+        private System.Windows.Forms.ToolStripMenuItem adminMenuOption;
+        private System.Windows.Forms.ToolStripMenuItem manageUsersMenuSubOption;
+        private System.Windows.Forms.ToolStripMenuItem viewUsersOption;
+        private System.Windows.Forms.ToolStripMenuItem registerNewUserOption;
+        private System.Windows.Forms.ToolStripMenuItem statisticsMenuSubOption;
+        private System.Windows.Forms.ToolStripMenuItem viewGraphsIOption;
+        private System.Windows.Forms.ToolStripMenuItem viewReportsOption;
+        private System.Windows.Forms.ToolStripMenuItem logsMenuSubOption;
+        private System.Windows.Forms.ToolStripMenuItem viewActivityLogsOption;
+        private System.Windows.Forms.ToolStripMenuItem viewErrorLogsOption;
+        private System.Windows.Forms.Label LogOutLabel;
     }
 }
