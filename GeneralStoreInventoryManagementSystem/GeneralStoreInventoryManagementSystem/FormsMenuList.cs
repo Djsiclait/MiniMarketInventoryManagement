04c4521dad0a1c62c0b29a68d1bd03047ae4fc4c
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-01-23 10:12:29 +0100

    Add FormMenuList class to host the singleton instances of every from created

---
+++ GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/FormsMenuList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralStoreInventoryManagementSystem
+{
+    public class FormsMenuList
+    {
+        public static InventorySearchForm inventorySearchForm = new InventorySearchForm(); // This is to create the forms used to browse the inventory in search of a product
+    }
+}
--- GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/InventorySearchForm.Designer.cs
+++ GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/InventorySearchForm.Designer.cs
@@ -28,11 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(275, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // InventorySearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 462);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimumSize = new System.Drawing.Size(700, 500);
+            this.Name = "InventorySearchForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "InventorySearchForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
