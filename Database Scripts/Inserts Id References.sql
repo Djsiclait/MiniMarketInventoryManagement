6a69a592dcf0e1b6114d68a6b06b2c44dffd997f
djsiclait <djidjellypas92@gmail.com>
2020-01-22 21:39:00 -0400

    Create isert to register basic id refrence system

---
+++ Database Scripts/Inserts Id References.sql
@@ -0,0 +1,32 @@
+INSERT INTO tbl_id_refrence_logs
+(
+	fld_id_refrence_id,
+	fld_id_refrence_table
+)
+Values
+(
+	'PB',
+	'Product Brands'
+),
+(
+	'PC',
+	'Product Categories'
+),
+(
+	'PT',
+	'Product Types'
+),
+(
+	'P',
+	'Products'
+),
+(
+	'S',
+	'Sales'
+),
+(
+	'SU',
+	'Suppliers'
+)
+
+
