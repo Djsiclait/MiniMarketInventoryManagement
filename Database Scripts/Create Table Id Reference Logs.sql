9966c4381297870a458ca4b0d5b7b2c425b18a28
djsiclait <djidjellypas92@gmail.com>
2020-01-22 21:59:11 -0400

    Minor changes

--- Database Scripts/Create Table Id Reference Logs.sql
+++ Database Scripts/Create Table Id Reference Logs.sql
@@ -1,7 +1,7 @@
-CREATE TABLE Tbl_Id_Refrence_Logs
+CREATE TABLE Tbl_Id_Reference_Logs
 (
 	fld_id_refrence_id varchar(4),
-	fld_id_refrence_table varchar(100),
+	fld_id_refrence_table varchar(100) Unique,
 	fld_id_refrence_iteration int Default 0,
 
 	Primary Key (fld_id_refrence_id)
