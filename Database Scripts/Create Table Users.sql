b16b84f9d39eb110ed9183d1d7957ba9e8f9adc2
djsiclait <djidjellypas92@gmail.com>
2020-01-22 21:38:19 -0400

    Add user status to user entity

--- Database Scripts/Create Table Users.sql
+++ Database Scripts/Create Table Users.sql
@@ -4,6 +4,7 @@ CREATE TABLE Tbl_Users
 	fld_user_first_name varchar(100) Not Null,
 	fld_user_last_name varchar(100) Not Null,
 	fld_user_role varchar(100) Not Null,
+	fld_user_status tinyint Default 0, ---- 0 = Active, 1 = Inactive, 2 = Suspended
 
 	Primary Key (fld_user_username)
  )
