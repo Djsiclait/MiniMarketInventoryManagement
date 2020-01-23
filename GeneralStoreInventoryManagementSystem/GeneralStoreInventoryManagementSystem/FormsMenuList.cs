578fc24e1a8bc504249bfc7081472760120064d1
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-01-23 11:45:12 +0100

    Add sales record form in presentation page

--- GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/FormsMenuList.cs
+++ GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/FormsMenuList.cs
@@ -9,9 +9,14 @@ namespace GeneralStoreInventoryManagementSystem
     public class FormsMenuList
     {
         // This is to create the forms used to browse the inventory in search of a product
+        // This is not used to make add products to the cart nor register purchases or restock products
+        // This form displays the basic information of products such as name, brand, price, and number of units instock 
         public static InventorySearchForm inventorySearchForm = new InventorySearchForm();
         
         // This form is used to log in to begin a new user session 
         public static LoginForm loginForm = new LoginForm();
+
+        // This form is dedicated to view the recent sales depending the requested time interval
+        public static SalesRecordForm salesRecordForm = new SalesRecordForm();
     }
 }
--- GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/SalesRecordForm.Designer.cs
+++ GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/SalesRecordForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GeneralStoreInventoryManagementSystem
 {
-    partial class InventorySearchForm
+    partial class SalesRecordForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,15 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.InventorySearchButton = new System.Windows.Forms.Button();
-            this.inventorySearchBox = new System.Windows.Forms.TextBox();
-            this.browserFormTitle = new System.Windows.Forms.Label();
-            this.productList = new System.Windows.Forms.DataGridView();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.salesMenuOption = new System.Windows.Forms.ToolStripMenuItem();
-            this.viewSalesMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
-            this.makeSaleMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.makeSaleMenuOption = new System.Windows.Forms.ToolStripMenuItem();
             this.productsMenuOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.browseProductsMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
             this.registerNewProductMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
             this.restockProductsMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
             this.adminMenuOption = new System.Windows.Forms.ToolStripMenuItem();
@@ -49,88 +44,45 @@
             this.logsMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
             this.viewActivityLogsOption = new System.Windows.Forms.ToolStripMenuItem();
             this.viewErrorLogsOption = new System.Windows.Forms.ToolStripMenuItem();
-            this.LogOutLabel = new System.Windows.Forms.Label();
             this.ViewCartLabel = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.productList)).BeginInit();
+            this.LogOutLabel = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // InventorySearchButton
-            // 
-            this.InventorySearchButton.Location = new System.Drawing.Point(590, 41);
-            this.InventorySearchButton.Name = "InventorySearchButton";
-            this.InventorySearchButton.Size = new System.Drawing.Size(75, 23);
-            this.InventorySearchButton.TabIndex = 0;
-            this.InventorySearchButton.Text = "Search";
-            this.InventorySearchButton.UseVisualStyleBackColor = true;
-            // 
-            // inventorySearchBox
-            // 
-            this.inventorySearchBox.Location = new System.Drawing.Point(339, 41);
-            this.inventorySearchBox.Name = "inventorySearchBox";
-            this.inventorySearchBox.Size = new System.Drawing.Size(245, 20);
-            this.inventorySearchBox.TabIndex = 1;
-            // 
-            // browserFormTitle
-            // 
-            this.browserFormTitle.AutoSize = true;
-            this.browserFormTitle.Location = new System.Drawing.Point(66, 46);
-            this.browserFormTitle.Name = "browserFormTitle";
-            this.browserFormTitle.Size = new System.Drawing.Size(92, 13);
-            this.browserFormTitle.TabIndex = 2;
-            this.browserFormTitle.Text = "Inventory Browser";
-            // 
-            // productList
-            // 
-            this.productList.AllowUserToOrderColumns = true;
-            this.productList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.productList.Location = new System.Drawing.Point(12, 79);
-            this.productList.Name = "productList";
-            this.productList.Size = new System.Drawing.Size(653, 400);
-            this.productList.TabIndex = 3;
             // 
             // menuStrip1
             // 
             this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.salesMenuOption,
+            this.makeSaleMenuOption,
             this.productsMenuOption,
             this.adminMenuOption});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(684, 24);
-            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.TabIndex = 5;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // salesMenuOption
+            // makeSaleMenuOption
             // 
-            this.salesMenuOption.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.viewSalesMenuSubOption,
-            this.makeSaleMenuSubOption});
-            this.salesMenuOption.Name = "salesMenuOption";
-            this.salesMenuOption.Size = new System.Drawing.Size(45, 20);
-            this.salesMenuOption.Text = "Sales";
-            // 
-            // viewSalesMenuSubOption
-            // 
-            this.viewSalesMenuSubOption.Name = "viewSalesMenuSubOption";
-            this.viewSalesMenuSubOption.Size = new System.Drawing.Size(152, 22);
-            this.viewSalesMenuSubOption.Text = "View Sales";
-            // 
-            // makeSaleMenuSubOption
-            // 
-            this.makeSaleMenuSubOption.Name = "makeSaleMenuSubOption";
-            this.makeSaleMenuSubOption.Size = new System.Drawing.Size(152, 22);
-            this.makeSaleMenuSubOption.Text = "Make Sale";
+            this.makeSaleMenuOption.Name = "makeSaleMenuOption";
+            this.makeSaleMenuOption.Size = new System.Drawing.Size(72, 20);
+            this.makeSaleMenuOption.Text = "Make Sale";
             // 
             // productsMenuOption
             // 
             this.productsMenuOption.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.browseProductsMenuSubOption,
             this.registerNewProductMenuSubOption,
             this.restockProductsMenuSubOption});
             this.productsMenuOption.Name = "productsMenuOption";
             this.productsMenuOption.Size = new System.Drawing.Size(66, 20);
             this.productsMenuOption.Text = "Products";
+            // 
+            // browseProductsMenuSubOption
+            // 
+            this.browseProductsMenuSubOption.Name = "browseProductsMenuSubOption";
+            this.browseProductsMenuSubOption.Size = new System.Drawing.Size(188, 22);
+            this.browseProductsMenuSubOption.Text = "Browse Products";
             // 
             // registerNewProductMenuSubOption
             // 
@@ -217,16 +169,6 @@
             this.viewErrorLogsOption.Size = new System.Drawing.Size(170, 22);
             this.viewErrorLogsOption.Text = "View Error Logs";
             // 
-            // LogOutLabel
-            // 
-            this.LogOutLabel.AutoSize = true;
-            this.LogOutLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.LogOutLabel.Location = new System.Drawing.Point(627, 9);
-            this.LogOutLabel.Name = "LogOutLabel";
-            this.LogOutLabel.Size = new System.Drawing.Size(45, 13);
-            this.LogOutLabel.TabIndex = 5;
-            this.LogOutLabel.Text = "Log Out";
-            // 
             // ViewCartLabel
             // 
             this.ViewCartLabel.AutoSize = true;
@@ -234,28 +176,29 @@
             this.ViewCartLabel.Location = new System.Drawing.Point(522, 9);
             this.ViewCartLabel.Name = "ViewCartLabel";
             this.ViewCartLabel.Size = new System.Drawing.Size(52, 13);
-            this.ViewCartLabel.TabIndex = 6;
+            this.ViewCartLabel.TabIndex = 8;
             this.ViewCartLabel.Text = "View Cart";
             // 
-            // InventorySearchForm
+            // LogOutLabel
+            // 
+            this.LogOutLabel.AutoSize = true;
+            this.LogOutLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.LogOutLabel.Location = new System.Drawing.Point(627, 9);
+            this.LogOutLabel.Name = "LogOutLabel";
+            this.LogOutLabel.Size = new System.Drawing.Size(45, 13);
+            this.LogOutLabel.TabIndex = 7;
+            this.LogOutLabel.Text = "Log Out";
+            // 
+            // SalesRecordForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(684, 491);
+            this.ClientSize = new System.Drawing.Size(684, 462);
             this.Controls.Add(this.ViewCartLabel);
             this.Controls.Add(this.LogOutLabel);
-            this.Controls.Add(this.productList);
-            this.Controls.Add(this.browserFormTitle);
-            this.Controls.Add(this.inventorySearchBox);
-            this.Controls.Add(this.InventorySearchButton);
             this.Controls.Add(this.menuStrip1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MainMenuStrip = this.menuStrip1;
-            this.MinimumSize = new System.Drawing.Size(700, 500);
-            this.Name = "InventorySearchForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Product Browser";
-            ((System.ComponentModel.ISupportInitialize)(this.productList)).EndInit();
+            this.Name = "SalesRecordForm";
+            this.Text = "Sales Record";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -265,15 +208,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button InventorySearchButton;
-        private System.Windows.Forms.TextBox inventorySearchBox;
-        private System.Windows.Forms.Label browserFormTitle;
-        private System.Windows.Forms.DataGridView productList;
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem salesMenuOption;
-        private System.Windows.Forms.ToolStripMenuItem viewSalesMenuSubOption;
-        private System.Windows.Forms.ToolStripMenuItem makeSaleMenuSubOption;
+        private System.Windows.Forms.ToolStripMenuItem makeSaleMenuOption;
         private System.Windows.Forms.ToolStripMenuItem productsMenuOption;
+        private System.Windows.Forms.ToolStripMenuItem browseProductsMenuSubOption;
         private System.Windows.Forms.ToolStripMenuItem registerNewProductMenuSubOption;
         private System.Windows.Forms.ToolStripMenuItem restockProductsMenuSubOption;
         private System.Windows.Forms.ToolStripMenuItem adminMenuOption;
@@ -286,7 +224,7 @@
         private System.Windows.Forms.ToolStripMenuItem logsMenuSubOption;
         private System.Windows.Forms.ToolStripMenuItem viewActivityLogsOption;
         private System.Windows.Forms.ToolStripMenuItem viewErrorLogsOption;
-        private System.Windows.Forms.Label LogOutLabel;
         private System.Windows.Forms.Label ViewCartLabel;
+        private System.Windows.Forms.Label LogOutLabel;
     }
 }
