b3af36db6ec1ab2f5d73f9577fcc3d3c6d3b1d9a
djsiclait <djidjellypas92@gmail.com>
2020-01-22 22:33:44 -0400

    Modify user entity to enclude creator, registration date, and last login timestamp

--- Database Scripts/Create Table Users.sql
+++ Database Scripts/Create Table Users.sql
@@ -4,7 +4,10 @@ CREATE TABLE Tbl_Users
 	fld_user_first_name varchar(100) Not Null,
 	fld_user_last_name varchar(100) Not Null,
 	fld_user_role varchar(100) Not Null,
+	fld_user_creator varchar(50) Default 'super.admin',
+	fld_user_registration_date datetime Default GETDATE(),
 	fld_user_status tinyint Default 0, ---- 0 = Active, 1 = Inactive, 2 = Suspended
+	fld_user_last_login_timestamp datetime, 
 
 	Primary Key (fld_user_username)
  )
