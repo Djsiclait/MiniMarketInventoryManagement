186f8bbc843753ae0a2fe04e99e2896da4501998
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-01-23 16:36:52 +0100

    Add menu bar logic to activity logs form

--- GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/ActivitiesLogForm.Designer.cs
+++ GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/ActivitiesLogForm.Designer.cs
@@ -28,46 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.ViewCartLabel = new System.Windows.Forms.Label();
-            this.LogOutLabel = new System.Windows.Forms.Label();
+            this.viewCartLabel = new System.Windows.Forms.Label();
+            this.logOutLabel = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.salesMenuOption = new System.Windows.Forms.ToolStripMenuItem();
             this.viewSalesMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
             this.makeSaleMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.productsMenuOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.browseProductsMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.registerNewProductMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.restockProductsMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
             this.adminMenuOption = new System.Windows.Forms.ToolStripMenuItem();
             this.manageUsersMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
             this.viewUsersOption = new System.Windows.Forms.ToolStripMenuItem();
             this.registerNewUserOption = new System.Windows.Forms.ToolStripMenuItem();
             this.statisticsMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
-            this.viewGraphsIOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewGraphsOption = new System.Windows.Forms.ToolStripMenuItem();
             this.viewReportsOption = new System.Windows.Forms.ToolStripMenuItem();
             this.viewErrorLogsMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
-            this.productsMenuOption = new System.Windows.Forms.ToolStripMenuItem();
-            this.browseProductsMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
-            this.registerNewProductMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
-            this.restockProductsMenuSubOption = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // ViewCartLabel
+            // viewCartLabel
             // 
-            this.ViewCartLabel.AutoSize = true;
-            this.ViewCartLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ViewCartLabel.Location = new System.Drawing.Point(522, 9);
-            this.ViewCartLabel.Name = "ViewCartLabel";
-            this.ViewCartLabel.Size = new System.Drawing.Size(52, 13);
-            this.ViewCartLabel.TabIndex = 9;
-            this.ViewCartLabel.Text = "View Cart";
+            this.viewCartLabel.AutoSize = true;
+            this.viewCartLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.viewCartLabel.Location = new System.Drawing.Point(522, 9);
+            this.viewCartLabel.Name = "viewCartLabel";
+            this.viewCartLabel.Size = new System.Drawing.Size(52, 13);
+            this.viewCartLabel.TabIndex = 9;
+            this.viewCartLabel.Text = "View Cart";
+            this.viewCartLabel.Click += new System.EventHandler(this.ViewCartLabel_Click);
+            this.viewCartLabel.MouseLeave += new System.EventHandler(this.ViewCartLabel_MouseLeave);
+            this.viewCartLabel.MouseHover += new System.EventHandler(this.ViewCartLabel_MouseHover);
             // 
-            // LogOutLabel
+            // logOutLabel
             // 
-            this.LogOutLabel.AutoSize = true;
-            this.LogOutLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.LogOutLabel.Location = new System.Drawing.Point(627, 9);
-            this.LogOutLabel.Name = "LogOutLabel";
-            this.LogOutLabel.Size = new System.Drawing.Size(45, 13);
-            this.LogOutLabel.TabIndex = 8;
-            this.LogOutLabel.Text = "Log Out";
+            this.logOutLabel.AutoSize = true;
+            this.logOutLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.logOutLabel.Location = new System.Drawing.Point(627, 9);
+            this.logOutLabel.Name = "logOutLabel";
+            this.logOutLabel.Size = new System.Drawing.Size(45, 13);
+            this.logOutLabel.TabIndex = 8;
+            this.logOutLabel.Text = "Log Out";
+            this.logOutLabel.Click += new System.EventHandler(this.LogOutLabel_Click);
+            this.logOutLabel.MouseLeave += new System.EventHandler(this.LogOutLabel_MouseLeave);
+            this.logOutLabel.MouseHover += new System.EventHandler(this.LogOutLabel_MouseHover);
             // 
             // menuStrip1
             // 
@@ -96,12 +102,45 @@
             this.viewSalesMenuSubOption.Name = "viewSalesMenuSubOption";
             this.viewSalesMenuSubOption.Size = new System.Drawing.Size(128, 22);
             this.viewSalesMenuSubOption.Text = "View Sales";
+            this.viewSalesMenuSubOption.Click += new System.EventHandler(this.ViewSalesMenuSubOption_Click);
             // 
             // makeSaleMenuSubOption
             // 
             this.makeSaleMenuSubOption.Name = "makeSaleMenuSubOption";
             this.makeSaleMenuSubOption.Size = new System.Drawing.Size(128, 22);
             this.makeSaleMenuSubOption.Text = "Make Sale";
+            this.makeSaleMenuSubOption.Click += new System.EventHandler(this.MakeSaleMenuSubOption_Click);
+            // 
+            // productsMenuOption
+            // 
+            this.productsMenuOption.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.browseProductsMenuSubOption,
+            this.registerNewProductMenuSubOption,
+            this.restockProductsMenuSubOption});
+            this.productsMenuOption.Name = "productsMenuOption";
+            this.productsMenuOption.Size = new System.Drawing.Size(66, 20);
+            this.productsMenuOption.Text = "Products";
+            // 
+            // browseProductsMenuSubOption
+            // 
+            this.browseProductsMenuSubOption.Name = "browseProductsMenuSubOption";
+            this.browseProductsMenuSubOption.Size = new System.Drawing.Size(188, 22);
+            this.browseProductsMenuSubOption.Text = "Browse Products";
+            this.browseProductsMenuSubOption.Click += new System.EventHandler(this.BrowseProductsMenuSubOption_Click);
+            // 
+            // registerNewProductMenuSubOption
+            // 
+            this.registerNewProductMenuSubOption.Name = "registerNewProductMenuSubOption";
+            this.registerNewProductMenuSubOption.Size = new System.Drawing.Size(188, 22);
+            this.registerNewProductMenuSubOption.Text = "Register New Product";
+            this.registerNewProductMenuSubOption.Click += new System.EventHandler(this.RegisterNewProductMenuSubOption_Click);
+            // 
+            // restockProductsMenuSubOption
+            // 
+            this.restockProductsMenuSubOption.Name = "restockProductsMenuSubOption";
+            this.restockProductsMenuSubOption.Size = new System.Drawing.Size(188, 22);
+            this.restockProductsMenuSubOption.Text = "Restock Products";
+            this.restockProductsMenuSubOption.Click += new System.EventHandler(this.RestockProductsMenuSubOption_Click);
             // 
             // adminMenuOption
             // 
@@ -127,75 +166,52 @@
             this.viewUsersOption.Name = "viewUsersOption";
             this.viewUsersOption.Size = new System.Drawing.Size(169, 22);
             this.viewUsersOption.Text = "View Users";
+            this.viewUsersOption.Click += new System.EventHandler(this.ViewUsersOption_Click);
             // 
             // registerNewUserOption
             // 
             this.registerNewUserOption.Name = "registerNewUserOption";
             this.registerNewUserOption.Size = new System.Drawing.Size(169, 22);
             this.registerNewUserOption.Text = "Register New User";
+            this.registerNewUserOption.Click += new System.EventHandler(this.RegisterNewUserOption_Click);
             // 
             // statisticsMenuSubOption
             // 
             this.statisticsMenuSubOption.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.viewGraphsIOption,
+            this.viewGraphsOption,
             this.viewReportsOption});
             this.statisticsMenuSubOption.Name = "statisticsMenuSubOption";
             this.statisticsMenuSubOption.Size = new System.Drawing.Size(155, 22);
             this.statisticsMenuSubOption.Text = "Statistics";
             // 
-            // viewGraphsIOption
+            // viewGraphsOption
             // 
-            this.viewGraphsIOption.Name = "viewGraphsIOption";
-            this.viewGraphsIOption.Size = new System.Drawing.Size(142, 22);
-            this.viewGraphsIOption.Text = "View Graphs";
+            this.viewGraphsOption.Name = "viewGraphsOption";
+            this.viewGraphsOption.Size = new System.Drawing.Size(142, 22);
+            this.viewGraphsOption.Text = "View Graphs";
+            this.viewGraphsOption.Click += new System.EventHandler(this.ViewGraphsOption_Click);
             // 
             // viewReportsOption
             // 
             this.viewReportsOption.Name = "viewReportsOption";
             this.viewReportsOption.Size = new System.Drawing.Size(142, 22);
             this.viewReportsOption.Text = "View Reports";
+            this.viewReportsOption.Click += new System.EventHandler(this.ViewReportsOption_Click);
             // 
             // viewErrorLogsMenuSubOption
             // 
             this.viewErrorLogsMenuSubOption.Name = "viewErrorLogsMenuSubOption";
             this.viewErrorLogsMenuSubOption.Size = new System.Drawing.Size(155, 22);
             this.viewErrorLogsMenuSubOption.Text = "View Error Logs";
-            // 
-            // productsMenuOption
-            // 
-            this.productsMenuOption.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.browseProductsMenuSubOption,
-            this.registerNewProductMenuSubOption,
-            this.restockProductsMenuSubOption});
-            this.productsMenuOption.Name = "productsMenuOption";
-            this.productsMenuOption.Size = new System.Drawing.Size(66, 20);
-            this.productsMenuOption.Text = "Products";
-            // 
-            // browseProductsMenuSubOption
-            // 
-            this.browseProductsMenuSubOption.Name = "browseProductsMenuSubOption";
-            this.browseProductsMenuSubOption.Size = new System.Drawing.Size(188, 22);
-            this.browseProductsMenuSubOption.Text = "Browse Products";
-            // 
-            // registerNewProductMenuSubOption
-            // 
-            this.registerNewProductMenuSubOption.Name = "registerNewProductMenuSubOption";
-            this.registerNewProductMenuSubOption.Size = new System.Drawing.Size(188, 22);
-            this.registerNewProductMenuSubOption.Text = "Register New Product";
-            // 
-            // restockProductsMenuSubOption
-            // 
-            this.restockProductsMenuSubOption.Name = "restockProductsMenuSubOption";
-            this.restockProductsMenuSubOption.Size = new System.Drawing.Size(188, 22);
-            this.restockProductsMenuSubOption.Text = "Restock Products";
+            this.viewErrorLogsMenuSubOption.Click += new System.EventHandler(this.ViewErrorLogsMenuSubOption_Click);
             // 
             // ActivitiesLogForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(684, 462);
-            this.Controls.Add(this.ViewCartLabel);
-            this.Controls.Add(this.LogOutLabel);
+            this.Controls.Add(this.viewCartLabel);
+            this.Controls.Add(this.logOutLabel);
             this.Controls.Add(this.menuStrip1);
             this.Name = "ActivitiesLogForm";
             this.Text = "Activity Logs";
@@ -208,8 +224,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Label ViewCartLabel;
-        private System.Windows.Forms.Label LogOutLabel;
+        private System.Windows.Forms.Label viewCartLabel;
+        private System.Windows.Forms.Label logOutLabel;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem salesMenuOption;
         private System.Windows.Forms.ToolStripMenuItem viewSalesMenuSubOption;
@@ -219,7 +235,7 @@
         private System.Windows.Forms.ToolStripMenuItem viewUsersOption;
         private System.Windows.Forms.ToolStripMenuItem registerNewUserOption;
         private System.Windows.Forms.ToolStripMenuItem statisticsMenuSubOption;
-        private System.Windows.Forms.ToolStripMenuItem viewGraphsIOption;
+        private System.Windows.Forms.ToolStripMenuItem viewGraphsOption;
         private System.Windows.Forms.ToolStripMenuItem viewReportsOption;
         private System.Windows.Forms.ToolStripMenuItem viewErrorLogsMenuSubOption;
         private System.Windows.Forms.ToolStripMenuItem productsMenuOption;
