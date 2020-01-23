90e459ba9fe3a9d8494480d502c4d72511da4cf1
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-01-23 11:24:20 +0100

    Add log in form in presentation page

--- GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/FormsMenuList.cs
+++ GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/FormsMenuList.cs
@@ -8,6 +8,10 @@ namespace GeneralStoreInventoryManagementSystem
 {
     public class FormsMenuList
     {
-        public static InventorySearchForm inventorySearchForm = new InventorySearchForm(); // This is to create the forms used to browse the inventory in search of a product
+        // This is to create the forms used to browse the inventory in search of a product
+        public static InventorySearchForm inventorySearchForm = new InventorySearchForm();
+        
+        // This form is used to log in to begin a new user session 
+        public static LoginForm loginForm = new LoginForm();
     }
 }
