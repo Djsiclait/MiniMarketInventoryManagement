487cbdc1951ee4b11339e7adf1037909b1536ce4
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-01-28 14:03:15 +0100

    Create stored procedure to fetch registered users

---
+++ Database Scripts/Stored Procedure Fetch User List Data.sql
@@ -0,0 +1,41 @@
+CREATE PROC SP_Fetch_User_List_Data
+	@username varchar(50),
+	@user_permission varchar(100)
+AS
+	BEGIN
+		
+		IF @user_permission = 'Super'
+			Select
+				fld_user_username,
+				fld_user_first_name,
+				fld_user_last_name,
+				fld_user_role,
+				fld_user_last_login_timestamp,
+				fld_user_status
+			From
+				Tbl_Users
+			Where
+				fld_user_username <> @username
+		ELSE IF @user_permission = 'Admin'
+			Select
+				fld_user_username,
+				fld_user_first_name,
+				fld_user_last_name,
+				fld_user_role,
+				fld_user_last_login_timestamp
+			From
+				Tbl_Users
+			Where
+				fld_user_username <> @username
+			and
+				fld_user_username <> 'super.admin'
+
+	END
+
+go
+
+exec SP_Fetch_User_List_Data 'p.siclait', 'Admin'
+
+
+
+
