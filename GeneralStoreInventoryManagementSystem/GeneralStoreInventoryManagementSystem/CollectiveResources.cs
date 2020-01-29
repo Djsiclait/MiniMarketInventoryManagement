7fb05dc8b5999356049a89953021c585ee6dcc4e
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-01-29 15:41:28 +0100

    Create collective resources class for public global common resources across the system

---
+++ GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/CollectiveResources.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Custom Libraries
+using InventoryManagementBusinessLayer;
+using InventoryManagementEntityLayer;
+
+namespace GeneralStoreInventoryManagementSystem
+{
+    public static class CollectiveResources
+    {
+    }
+}
