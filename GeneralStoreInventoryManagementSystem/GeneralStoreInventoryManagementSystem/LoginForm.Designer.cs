f86e46c5f9ac70d9a0bbea62a3e765594bc70a8a
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-01-29 15:12:31 +0100

    Design login form interface

--- GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/LoginForm.Designer.cs
+++ GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/LoginForm.Designer.cs
@@ -28,19 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.loginTitleLabel = new System.Windows.Forms.Label();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.logInButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // loginTitleLabel
+            // 
+            this.loginTitleLabel.AutoSize = true;
+            this.loginTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginTitleLabel.Location = new System.Drawing.Point(217, 9);
+            this.loginTitleLabel.Name = "loginTitleLabel";
+            this.loginTitleLabel.Size = new System.Drawing.Size(53, 17);
+            this.loginTitleLabel.TabIndex = 0;
+            this.loginTitleLabel.Text = "Log In";
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(48, 35);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(58, 13);
+            this.usernameLabel.TabIndex = 1;
+            this.usernameLabel.Text = "Username:";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(84, 51);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(298, 20);
+            this.usernameTextBox.TabIndex = 2;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.AutoSize = true;
+            this.passwordTextBox.Location = new System.Drawing.Point(48, 85);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(56, 13);
+            this.passwordTextBox.TabIndex = 3;
+            this.passwordTextBox.Text = "Password:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(217, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // logInButton
+            // 
+            this.logInButton.Location = new System.Drawing.Point(252, 139);
+            this.logInButton.Name = "logInButton";
+            this.logInButton.Size = new System.Drawing.Size(130, 23);
+            this.logInButton.TabIndex = 5;
+            this.logInButton.Text = "Connect to System";
+            this.logInButton.UseVisualStyleBackColor = true;
             // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 262);
+            this.ClientSize = new System.Drawing.Size(457, 179);
+            this.Controls.Add(this.logInButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.loginTitleLabel);
+            this.MaximizeBox = false;
             this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label loginTitleLabel;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.Label passwordTextBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button logInButton;
     }
 }
