42dba55f6c4d22aa010d11a5cb8282a33868c357
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-01-30 10:52:23 +0100

    Create activity clas in entity layer

---
+++ GeneralStoreInventoryManagementSystem/InventoryManagementEntityLayer/Activity.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementEntityLayer
+{
+    public class Activity
+    {
+        private String username;
+        private String description;
+        private String type;
+        private DateTime timestamp;
+
+        // Getters and Setters
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+
+            set
+            {
+                username = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+
+            set
+            {
+                description = value;
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+
+            set
+            {
+                type = value;
+            }
+        }
+
+        public DateTime Timestamp
+        {
+            get
+            {
+                return timestamp;
+            }
+
+            set
+            {
+                timestamp = value;
+            }
+        }
+    }
+}
