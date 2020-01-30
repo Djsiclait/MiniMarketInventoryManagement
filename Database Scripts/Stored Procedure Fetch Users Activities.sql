584cca372ee25d59d331edffe6b11a828d85fa2b
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-01-30 10:48:56 +0100

    Create stored procedure to fetch users' activities

---
+++ Database Scripts/Stored Procedure Fetch Users Activities.sql
@@ -0,0 +1,32 @@
+CREATE PROC SP_Fetch_Users_Activities_Data
+	@user_permission varchar(100)
+AS
+	BEGIN
+		
+		IF @user_permission = 'Super'
+			Select
+				fld_user_activity_username,
+				fld_user_activity_description,
+				fld_user_activity_type,
+				fld_user_activity_timestamp
+			From
+				Tbl_User_Activity_Logs
+		ELSE IF @user_permission = 'Admin'
+			Select
+				fld_user_activity_username,
+				fld_user_activity_description,
+				fld_user_activity_type,
+				fld_user_activity_timestamp
+			From
+				Tbl_User_Activity_Logs
+			Where
+				fld_user_activity_username <> 'super.admin'
+
+	END
+
+go
+
+exec SP_Fetch_Users_Activities_Data 'Admin'
+
+
+
---
+++ Database Scripts/temporary activity insert.sql
@@ -0,0 +1,34 @@
+Insert Into Tbl_User_Activity_Logs
+(
+	fld_user_activity_username,
+	fld_user_activity_description,
+	fld_user_activity_type
+)
+Values
+(
+	'p.siclait',
+	'User, p.siclait, logged into the system successfully',
+	'LOG IN'
+),
+(
+	'p.siclait',
+	'User, p.siclait, viewed the inventory browser',
+	'LOG IN'
+),
+(
+	'p.siclait',
+	'Error occured while registering new product',
+	'ERROR'
+),
+(
+	'p.siclait',
+	'User, p.siclait, logged out the system successfully',
+	'LOG OUT'
+)
+
+
+
+
+
+
+
