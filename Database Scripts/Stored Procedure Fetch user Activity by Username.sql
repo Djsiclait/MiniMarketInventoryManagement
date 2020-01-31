a0056bf79a22ab9528fbf6a14716024ed62f2972
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-01-31 10:25:10 +0100

    Create stored procedure to fetch the activiies of one specific use by his/her username

---
+++ Database Scripts/Stored Procedure Fetch user Activity by Username.sql
@@ -0,0 +1,62 @@
+CREATE PROC SP_Fetch_User_Activity_By_Username
+	@user_permission varchar(100),
+	@username varchar(50),
+	@key_word varchar(100)
+AS
+	BEGIN
+		
+		IF @user_permission = 'Super'
+			Select
+				fld_user_activity_username,
+				fld_user_activity_description,
+				fld_user_activity_type,
+				fld_user_activity_timestamp
+			From
+				Tbl_User_Activity_Logs
+			Where
+				fld_user_activity_username = @username
+			and
+				(
+				fld_user_activity_username Like '%' + @key_word + '%' or 
+				fld_user_activity_description Like '%' + @key_word + '%' or
+				fld_user_activity_type Like '%' + @key_word + '%' or
+				fld_user_activity_timestamp Like '%' + @key_word + '%'
+				)
+			Order by 
+				fld_user_activity_timestamp Desc
+		ELSE IF @user_permission = 'Admin'
+			Select
+				fld_user_activity_username,
+				fld_user_activity_description,
+				fld_user_activity_type,
+				fld_user_activity_timestamp
+			From
+				Tbl_User_Activity_Logs
+			Where
+				fld_user_activity_username = @username
+			and
+				fld_user_activity_username <> 'super.admin'
+			and
+				(
+				fld_user_activity_username Like '%' + @key_word + '%' or 
+				fld_user_activity_description Like '%' + @key_word + '%' or
+				fld_user_activity_type Like '%' + @key_word + '%' or
+				fld_user_activity_timestamp Like '%' + @key_word + '%'
+				)
+			Order by 
+				fld_user_activity_timestamp Desc
+		
+	END
+
+go
+
+exec SP_Fetch_User_Activity_By_Username 'Admin', 'b.siclait', ''
+
+
+
+
+
+
+
+
+
