e7cf968ac01e5308ef64e3308fff973111e3e8bc
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-02-01 12:48:00 +0100

    Create stored procedure to fetch brand names

---
+++ Database Scripts/Stored Procedure Fetch Registered Brand Names.sql
@@ -0,0 +1,27 @@
+CREATE PROC SP_Fetch_Registered_Brand_Names_Data
+	@key_word varchar(100)
+AS
+	BEGIN
+
+		Select 
+			fld_brand_name
+		From
+			Tbl_Product_Brands
+		Where
+			fld_brand_name <> ''
+		and
+			fld_brand_name like '%' + @key_word + '%'
+	END
+
+go
+
+exec SP_Fetch_Registered_Brand_Names_Data ''
+
+
+
+
+
+
+
+
+
