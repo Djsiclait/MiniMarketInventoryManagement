e2ee95cc38453529a5fa0011e9cfc5b6f78c64c7
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-02-01 12:35:53 +0100

    Create brand class in entity layer

---
+++ GeneralStoreInventoryManagementSystem/InventoryManagementEntityLayer/Brand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementEntityLayer
+{
+    public class Brand
+    {
+        private String id;
+        private String name;
+
+        // Getters and Setters
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+    }
+}
