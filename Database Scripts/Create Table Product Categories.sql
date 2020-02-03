77a9a4f7f2475232978d7f9434ea0eae93a8ba3a
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-02-03 23:31:04 +0100

    Create stored procedure to register new product category

--- Database Scripts/Create Table Product Categories.sql
+++ Database Scripts/Create Table Product Categories.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Tbl_Product_Categories
 (
 	fld_category_id varchar(10) NOT NULL, -- Automatically generated identification code for product categories
-	fld_category_description varchar(50), -- name of product brand for detailed categorization and grouping purposes 
+	fld_category_description varchar(50) Unique, -- name of product brand for detailed categorization and grouping purposes 
 
 	PRIMARY KEY (fld_category_id)
 )
---
+++ Database Scripts/Stored Procedure Register New Category Data.sql
@@ -0,0 +1,72 @@
+CREATE PROC SP_Register_New_Category_Data
+	@description varchar(50),
+	@message varchar(300) output
+AS
+	BEGIN
+	
+		IF exists(select 1 from Tbl_Product_Categories where fld_category_description = @description)
+			set @message = 'This category is already registered'
+		ELSE 
+			BEGIN
+
+				Declare @new_category_id varchar(10)
+				exec SP_Generate_New_Id 'Product Categories', @new_category_id output
+		
+				Insert Into Tbl_Product_Categories
+				(
+					fld_category_id,
+					fld_category_description
+				)
+				Values 
+				(
+					@new_category_id,
+					@description
+				)
+
+				set @message = 'Supplier has been registered successfully!'
+
+			END
+	END
+
+go
+
+Declare @message varchar(300)
+
+exec SP_Register_New_Category_Data 'Lacteo', @message output
+select @message 
+exec SP_Register_New_Category_Data 'Bebida', @message output
+select @message 
+exec SP_Register_New_Category_Data 'Bebe', @message output
+select @message 
+exec SP_Register_New_Category_Data 'Cereal', @message output
+select @message 
+exec SP_Register_New_Category_Data 'Pasta', @message output
+select @message 
+exec SP_Register_New_Category_Data 'Bebida Alcoholicas', @message output
+select @message 
+exec SP_Register_New_Category_Data 'Galleta', @message output
+select @message 
+exec SP_Register_New_Category_Data 'Panaderia', @message output
+select @message 
+exec SP_Register_New_Category_Data 'Carne', @message output
+select @message 
+exec SP_Register_New_Category_Data 'Embutido', @message output
+select @message 
+exec SP_Register_New_Category_Data 'Pescado', @message output
+select @message 
+exec SP_Register_New_Category_Data 'Vegetal', @message output
+select @message 
+exec SP_Register_New_Category_Data 'Frutas', @message output
+select @message 
+
+select * from Tbl_Product_Categories
+
+
+
+
+
+
+
+
+
+
