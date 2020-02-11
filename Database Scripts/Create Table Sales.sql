16486eb70aaba35d6d8f0a38c91aa79f0c85c4ba
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-02-11 12:07:49 +0100

    Modify diagrams to take into account sales status

--- Database Scripts/Create Table Sales.sql
+++ Database Scripts/Create Table Sales.sql
@@ -4,8 +4,9 @@ CREATE TABLE tbl_Sales
 	fld_sale_quantity_items int Default 1,
 	fld_sale_total decimal(19,0) Default 0,
 	fld_sale_delivery bit Default 0, ---- 0 = In store sale; 1 = Delivery 
-	fld_sale_date datetime NoT Null,
+	fld_sale_date datetime Default GETDATE(),
 	fld_sold_by varchar(50) Not Null, ---- User that completed the sale
+	fld_sale_status tinyint Default 0,
 
 	Primary Key (fld_sale_id)
 )
