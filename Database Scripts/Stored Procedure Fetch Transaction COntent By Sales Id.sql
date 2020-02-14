f6a08b1b25f806db9a2d02a7f37dced09b58e80b
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-02-14 10:36:29 +0100

    Create stored procedure to fetch transaction content

---
+++ Database Scripts/Stored Procedure Fetch Transaction COntent By Sales Id.sql
@@ -0,0 +1,45 @@
+CREATE PROC SP_Fetch_Transaction_Content_By_Sales_Id
+	@sales_id varchar(10)
+AS
+	BEGIN
+		
+		Select 
+			B.fld_product_name,
+			C.fld_brand_name,
+			A.fld_product_quantity,
+			A.fld_product_unit_price,
+			(A.fld_product_quantity * A.fld_product_unit_price) AS total
+		From
+			Tbl_Product_Sales as A
+		left join Tbl_Products as B on B.fld_product_id = A.fld_product_id 
+		left join Tbl_Product_Brands as C on C.fld_brand_id = B.fld_product_brand
+		Where
+			fld_sale_id = @sales_id
+
+	END
+
+go
+
+exec SP_Fetch_Transaction_Content_By_Sales_Id 'S11'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
