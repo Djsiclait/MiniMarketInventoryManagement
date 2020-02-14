9a8582c0acff0de55301cdd16fde0139b3f15b6b
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-02-14 12:06:16 +0100

    Create form to view transaction summary

---
+++ GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/SaleInformationTemplateForm.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace GeneralStoreInventoryManagementSystem
+{
+    partial class SaleInformationTemplateForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.identificationNumberLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saleIdLabel = new System.Windows.Forms.Label();
+            this.saleDateLabel = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.numberOfItemsLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.soldByLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.deliveryCheckBox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // identificationNumberLabel
+            // 
+            this.identificationNumberLabel.AutoSize = true;
+            this.identificationNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.identificationNumberLabel.Location = new System.Drawing.Point(27, 46);
+            this.identificationNumberLabel.Name = "identificationNumberLabel";
+            this.identificationNumberLabel.Size = new System.Drawing.Size(97, 13);
+            this.identificationNumberLabel.TabIndex = 0;
+            this.identificationNumberLabel.Text = "Identification #:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Transaction Date:";
+            // 
+            // saleIdLabel
+            // 
+            this.saleIdLabel.AutoSize = true;
+            this.saleIdLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saleIdLabel.Location = new System.Drawing.Point(53, 63);
+            this.saleIdLabel.Name = "saleIdLabel";
+            this.saleIdLabel.Size = new System.Drawing.Size(46, 17);
+            this.saleIdLabel.TabIndex = 2;
+            this.saleIdLabel.Text = "label2";
+            // 
+            // saleDateLabel
+            // 
+            this.saleDateLabel.AutoSize = true;
+            this.saleDateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saleDateLabel.Location = new System.Drawing.Point(53, 112);
+            this.saleDateLabel.Name = "saleDateLabel";
+            this.saleDateLabel.Size = new System.Drawing.Size(46, 17);
+            this.saleDateLabel.TabIndex = 3;
+            this.saleDateLabel.Text = "label2";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(229, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(393, 281);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // numberOfItemsLabel
+            // 
+            this.numberOfItemsLabel.AutoSize = true;
+            this.numberOfItemsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberOfItemsLabel.Location = new System.Drawing.Point(229, 301);
+            this.numberOfItemsLabel.Name = "numberOfItemsLabel";
+            this.numberOfItemsLabel.Size = new System.Drawing.Size(131, 17);
+            this.numberOfItemsLabel.TabIndex = 5;
+            this.numberOfItemsLabel.Text = "Number of Items:";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLabel.Location = new System.Drawing.Point(457, 301);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(96, 17);
+            this.totalLabel.TabIndex = 6;
+            this.totalLabel.Text = "Total: $0.00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Sold By:";
+            // 
+            // soldByLabel
+            // 
+            this.soldByLabel.AutoSize = true;
+            this.soldByLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.soldByLabel.Location = new System.Drawing.Point(56, 166);
+            this.soldByLabel.Name = "soldByLabel";
+            this.soldByLabel.Size = new System.Drawing.Size(46, 17);
+            this.soldByLabel.TabIndex = 8;
+            this.soldByLabel.Text = "label3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(30, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Satus:";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.Location = new System.Drawing.Point(56, 216);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(46, 17);
+            this.statusLabel.TabIndex = 10;
+            this.statusLabel.Text = "label4";
+            // 
+            // deliveryCheckBox
+            // 
+            this.deliveryCheckBox.AutoSize = true;
+            this.deliveryCheckBox.Location = new System.Drawing.Point(33, 257);
+            this.deliveryCheckBox.Name = "deliveryCheckBox";
+            this.deliveryCheckBox.Size = new System.Drawing.Size(95, 17);
+            this.deliveryCheckBox.TabIndex = 11;
+            this.deliveryCheckBox.Text = "Was Delivery?";
+            this.deliveryCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // SaleInformationTemplateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 334);
+            this.Controls.Add(this.deliveryCheckBox);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.soldByLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.numberOfItemsLabel);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.saleDateLabel);
+            this.Controls.Add(this.saleIdLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.identificationNumberLabel);
+            this.Name = "SaleInformationTemplateForm";
+            this.Text = "Sale Information: ";
+            this.Load += new System.EventHandler(this.SaleInformationTemplateForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label identificationNumberLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label saleIdLabel;
+        private System.Windows.Forms.Label saleDateLabel;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label numberOfItemsLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label soldByLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.CheckBox deliveryCheckBox;
+    }
+}
---
+++ GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/SaleInformationTemplateForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Custom Library
+using InventoryManagementBusinessLayer;
+using InventoryManagementEntityLayer;
+
+namespace GeneralStoreInventoryManagementSystem
+{
+    public partial class SaleInformationTemplateForm : Form
+    {
+        Sale sale;
+
+        public SaleInformationTemplateForm(String saleId)
+        {
+            InitializeComponent();
+
+            DisplayTransactionInformation(saleId);
+        }
+
+        #region Form Load Logic
+        private void SaleInformationTemplateForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region Auxiliary Functions
+        private void DisplayTransactionInformation(String saleId)
+        {
+            sale = ConsultInformation.FetchTransactionInformationBySalesId(saleId);
+
+            saleIdLabel.Text = sale.Id;
+            saleDateLabel.Text = sale.TransactionDate.ToString();
+            soldByLabel.Text = sale.SoldBy;
+            statusLabel.Text = sale.Status;
+            deliveryCheckBox.Checked = sale.Delivery;
+        }
+        #endregion
+    }
+}
