e9663b29e562299a20b05459ec9f10b50126df28
djsiclait <djidjellypas92@gmail.com>
2020-02-17 22:13:28 -0400

    Create classes to manage sales data and information in business and data layer

---
+++ GeneralStoreInventoryManagementSystem/InventoryManagementBusinessLayer/SaleInformationManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Custom Libraries
+using InventoryManagementDataLayer;
+using InventoryManagementEntityLayer;
+
+namespace InventoryManagementBusinessLayer
+{
+    public static class SaleInformationManager
+    {
+    }
+}
---
+++ GeneralStoreInventoryManagementSystem/InventoryManagementDataLayer/SaleDataManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+// Custome Library
+using InventoryManagementEntityLayer;
+
+namespace InventoryManagementDataLayer
+{
+    public static class SaleDataManager
+    {
+    }
+}
