8809fd4f557d735e92707ffd7b1980c6b92676d4
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-02-22 14:48:30 +0100

    Establish spanish and french localization in transaction summary mini form

--- GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/SaleInformationTemplateForm.Designer.cs
+++ GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/SaleInformationTemplateForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SaleInformationTemplateForm));
             this.identificationNumberLabel = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.saleIdLabel = new System.Windows.Forms.Label();
@@ -54,229 +55,128 @@
             // 
             // identificationNumberLabel
             // 
-            this.identificationNumberLabel.AutoSize = true;
-            this.identificationNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.identificationNumberLabel.Location = new System.Drawing.Point(31, 53);
+            resources.ApplyResources(this.identificationNumberLabel, "identificationNumberLabel");
             this.identificationNumberLabel.Name = "identificationNumberLabel";
-            this.identificationNumberLabel.Size = new System.Drawing.Size(97, 13);
-            this.identificationNumberLabel.TabIndex = 0;
-            this.identificationNumberLabel.Text = "Identification #:";
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(31, 106);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(109, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Transaction Date:";
             // 
             // saleIdLabel
             // 
-            this.saleIdLabel.AutoSize = true;
-            this.saleIdLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.saleIdLabel.Location = new System.Drawing.Point(57, 70);
+            resources.ApplyResources(this.saleIdLabel, "saleIdLabel");
             this.saleIdLabel.Name = "saleIdLabel";
-            this.saleIdLabel.Size = new System.Drawing.Size(46, 17);
-            this.saleIdLabel.TabIndex = 2;
-            this.saleIdLabel.Text = "label2";
             // 
             // saleDateLabel
             // 
-            this.saleDateLabel.AutoSize = true;
-            this.saleDateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.saleDateLabel.Location = new System.Drawing.Point(57, 119);
+            resources.ApplyResources(this.saleDateLabel, "saleDateLabel");
             this.saleDateLabel.Name = "saleDateLabel";
-            this.saleDateLabel.Size = new System.Drawing.Size(46, 17);
-            this.saleDateLabel.TabIndex = 3;
-            this.saleDateLabel.Text = "label2";
             // 
             // contentDataGridView
             // 
+            resources.ApplyResources(this.contentDataGridView, "contentDataGridView");
             this.contentDataGridView.AllowUserToAddRows = false;
             this.contentDataGridView.AllowUserToDeleteRows = false;
             this.contentDataGridView.AllowUserToResizeColumns = false;
             this.contentDataGridView.AllowUserToResizeRows = false;
             this.contentDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.contentDataGridView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-            this.contentDataGridView.Location = new System.Drawing.Point(229, 13);
             this.contentDataGridView.Name = "contentDataGridView";
             this.contentDataGridView.ReadOnly = true;
             this.contentDataGridView.RowHeadersVisible = false;
-            this.contentDataGridView.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.contentDataGridView.Size = new System.Drawing.Size(567, 281);
-            this.contentDataGridView.TabIndex = 4;
             // 
             // numberOfItemsLabel
             // 
-            this.numberOfItemsLabel.AutoSize = true;
-            this.numberOfItemsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numberOfItemsLabel.Location = new System.Drawing.Point(229, 301);
+            resources.ApplyResources(this.numberOfItemsLabel, "numberOfItemsLabel");
             this.numberOfItemsLabel.Name = "numberOfItemsLabel";
-            this.numberOfItemsLabel.Size = new System.Drawing.Size(131, 17);
-            this.numberOfItemsLabel.TabIndex = 5;
-            this.numberOfItemsLabel.Text = "Number of Items:";
             // 
             // totalLabel
             // 
-            this.totalLabel.AutoSize = true;
-            this.totalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.totalLabel.Location = new System.Drawing.Point(457, 301);
+            resources.ApplyResources(this.totalLabel, "totalLabel");
             this.totalLabel.Name = "totalLabel";
-            this.totalLabel.Size = new System.Drawing.Size(96, 17);
-            this.totalLabel.TabIndex = 6;
-            this.totalLabel.Text = "Total: $0.00";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(31, 156);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 13);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Sold By:";
             // 
             // soldByLabel
             // 
-            this.soldByLabel.AutoSize = true;
-            this.soldByLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.soldByLabel.Location = new System.Drawing.Point(57, 169);
+            resources.ApplyResources(this.soldByLabel, "soldByLabel");
             this.soldByLabel.Name = "soldByLabel";
-            this.soldByLabel.Size = new System.Drawing.Size(46, 17);
-            this.soldByLabel.TabIndex = 8;
-            this.soldByLabel.Text = "label3";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(31, 202);
+            resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(43, 13);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "Satus:";
             // 
             // statusLabel
             // 
-            this.statusLabel.AutoSize = true;
-            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.statusLabel.Location = new System.Drawing.Point(57, 215);
+            resources.ApplyResources(this.statusLabel, "statusLabel");
             this.statusLabel.Name = "statusLabel";
-            this.statusLabel.Size = new System.Drawing.Size(46, 17);
-            this.statusLabel.TabIndex = 10;
-            this.statusLabel.Text = "label4";
             // 
             // deliveryCheckBox
             // 
-            this.deliveryCheckBox.AutoSize = true;
-            this.deliveryCheckBox.Location = new System.Drawing.Point(33, 345);
+            resources.ApplyResources(this.deliveryCheckBox, "deliveryCheckBox");
             this.deliveryCheckBox.Name = "deliveryCheckBox";
-            this.deliveryCheckBox.Size = new System.Drawing.Size(95, 17);
-            this.deliveryCheckBox.TabIndex = 11;
-            this.deliveryCheckBox.Text = "Was Delivery?";
             this.deliveryCheckBox.UseVisualStyleBackColor = true;
             // 
             // voidSaleButton
             // 
+            resources.ApplyResources(this.voidSaleButton, "voidSaleButton");
             this.voidSaleButton.BackColor = System.Drawing.Color.Red;
-            this.voidSaleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.voidSaleButton.ForeColor = System.Drawing.Color.Black;
-            this.voidSaleButton.Location = new System.Drawing.Point(229, 334);
             this.voidSaleButton.Name = "voidSaleButton";
-            this.voidSaleButton.Size = new System.Drawing.Size(147, 36);
-            this.voidSaleButton.TabIndex = 12;
-            this.voidSaleButton.Text = "Void Sale";
             this.voidSaleButton.UseVisualStyleBackColor = false;
             this.voidSaleButton.Click += new System.EventHandler(this.VoidSaleButton_Click);
             // 
             // returnItemsButton
             // 
-            this.returnItemsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.returnItemsButton.Location = new System.Drawing.Point(460, 334);
+            resources.ApplyResources(this.returnItemsButton, "returnItemsButton");
             this.returnItemsButton.Name = "returnItemsButton";
-            this.returnItemsButton.Size = new System.Drawing.Size(147, 36);
-            this.returnItemsButton.TabIndex = 13;
-            this.returnItemsButton.Text = "Return Items";
             this.returnItemsButton.UseVisualStyleBackColor = true;
             this.returnItemsButton.Click += new System.EventHandler(this.ReturnItemsButton_Click);
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(12, 13);
+            resources.ApplyResources(this.label4, "label4");
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(182, 20);
-            this.label4.TabIndex = 14;
-            this.label4.Text = "Transaction Summary";
             // 
             // parentLabel
             // 
-            this.parentLabel.AutoSize = true;
-            this.parentLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.parentLabel.Location = new System.Drawing.Point(31, 249);
+            resources.ApplyResources(this.parentLabel, "parentLabel");
             this.parentLabel.Name = "parentLabel";
-            this.parentLabel.Size = new System.Drawing.Size(52, 13);
-            this.parentLabel.TabIndex = 15;
-            this.parentLabel.Text = "Parent: ";
             // 
             // parentIdLabel
             // 
-            this.parentIdLabel.AutoSize = true;
-            this.parentIdLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.parentIdLabel.Location = new System.Drawing.Point(57, 262);
+            resources.ApplyResources(this.parentIdLabel, "parentIdLabel");
             this.parentIdLabel.Name = "parentIdLabel";
-            this.parentIdLabel.Size = new System.Drawing.Size(46, 17);
-            this.parentIdLabel.TabIndex = 16;
-            this.parentIdLabel.Text = "label5";
             // 
             // childIdLabel
             // 
-            this.childIdLabel.AutoSize = true;
-            this.childIdLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.childIdLabel.Location = new System.Drawing.Point(157, 262);
+            resources.ApplyResources(this.childIdLabel, "childIdLabel");
             this.childIdLabel.Name = "childIdLabel";
-            this.childIdLabel.Size = new System.Drawing.Size(46, 17);
-            this.childIdLabel.TabIndex = 18;
-            this.childIdLabel.Text = "label5";
             // 
             // childLabel
             // 
-            this.childLabel.AutoSize = true;
-            this.childLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.childLabel.Location = new System.Drawing.Point(134, 249);
+            resources.ApplyResources(this.childLabel, "childLabel");
             this.childLabel.Name = "childLabel";
-            this.childLabel.Size = new System.Drawing.Size(43, 13);
-            this.childLabel.TabIndex = 17;
-            this.childLabel.Text = "Child: ";
             // 
             // label5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(31, 292);
+            resources.ApplyResources(this.label5, "label5");
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(87, 13);
-            this.label5.TabIndex = 19;
-            this.label5.Text = "Last Modified:";
             // 
             // lastModifiedDateLabel
             // 
-            this.lastModifiedDateLabel.AutoSize = true;
-            this.lastModifiedDateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lastModifiedDateLabel.Location = new System.Drawing.Point(57, 305);
+            resources.ApplyResources(this.lastModifiedDateLabel, "lastModifiedDateLabel");
             this.lastModifiedDateLabel.Name = "lastModifiedDateLabel";
-            this.lastModifiedDateLabel.Size = new System.Drawing.Size(46, 17);
-            this.lastModifiedDateLabel.TabIndex = 20;
-            this.lastModifiedDateLabel.Text = "label2";
             // 
             // SaleInformationTemplateForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(808, 382);
             this.Controls.Add(this.lastModifiedDateLabel);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.childIdLabel);
@@ -299,9 +199,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.identificationNumberLabel);
             this.MaximizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(742, 372);
             this.Name = "SaleInformationTemplateForm";
-            this.Text = "Sale Information:";
             this.Load += new System.EventHandler(this.SaleInformationTemplateForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.contentDataGridView)).EndInit();
             this.ResumeLayout(false);
