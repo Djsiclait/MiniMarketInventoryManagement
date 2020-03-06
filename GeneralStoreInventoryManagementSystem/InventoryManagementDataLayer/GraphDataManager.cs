3b936379d628bcd65646d5f3992b691e613ef499
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-03-06 11:26:41 +0100

    Create new Graph data and information libraries with respective classes

---
+++ GeneralStoreInventoryManagementSystem/InventoryManagementDataLayer/GraphDataManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementDataLayer
+{
+    namespace GraphData
+    {
+        public static class GraphDataManager
+        {
+
+        }
+    }
+}
