2a1598c8fc1edc694139d857ef4b6b82c1bb0d2f
Djidjelly Siclait <djidjellypas92@gmail.com>
2020-03-07 10:11:07 +0100

    Add inicial locolization to new forms

--- GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/UserSalesRecordsTemplateForm.Designer.cs
+++ GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/UserSalesRecordsTemplateForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserSalesRecordsTemplateForm));
             this.printEntireButton = new System.Windows.Forms.Button();
             this.printCurrentButton = new System.Windows.Forms.Button();
             this.totalLabel = new System.Windows.Forms.Label();
@@ -48,161 +49,104 @@
             // 
             // printEntireButton
             // 
-            this.printEntireButton.Location = new System.Drawing.Point(409, 440);
+            resources.ApplyResources(this.printEntireButton, "printEntireButton");
             this.printEntireButton.Name = "printEntireButton";
-            this.printEntireButton.Size = new System.Drawing.Size(136, 23);
-            this.printEntireButton.TabIndex = 31;
-            this.printEntireButton.Text = "Print Entire Set ";
             this.printEntireButton.UseVisualStyleBackColor = true;
             // 
             // printCurrentButton
             // 
-            this.printCurrentButton.Location = new System.Drawing.Point(551, 440);
+            resources.ApplyResources(this.printCurrentButton, "printCurrentButton");
             this.printCurrentButton.Name = "printCurrentButton";
-            this.printCurrentButton.Size = new System.Drawing.Size(136, 23);
-            this.printCurrentButton.TabIndex = 30;
-            this.printCurrentButton.Text = "Print Current Set ";
             this.printCurrentButton.UseVisualStyleBackColor = true;
             // 
             // totalLabel
             // 
-            this.totalLabel.AutoSize = true;
-            this.totalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.totalLabel.Location = new System.Drawing.Point(15, 420);
+            resources.ApplyResources(this.totalLabel, "totalLabel");
             this.totalLabel.Name = "totalLabel";
-            this.totalLabel.Size = new System.Drawing.Size(51, 13);
-            this.totalLabel.TabIndex = 29;
-            this.totalLabel.Text = "Total: $";
             // 
             // salesContentDataGridView
             // 
+            resources.ApplyResources(this.salesContentDataGridView, "salesContentDataGridView");
             this.salesContentDataGridView.AllowUserToAddRows = false;
             this.salesContentDataGridView.AllowUserToDeleteRows = false;
             this.salesContentDataGridView.AllowUserToResizeColumns = false;
             this.salesContentDataGridView.AllowUserToResizeRows = false;
             this.salesContentDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.salesContentDataGridView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-            this.salesContentDataGridView.Location = new System.Drawing.Point(15, 238);
             this.salesContentDataGridView.MultiSelect = false;
             this.salesContentDataGridView.Name = "salesContentDataGridView";
             this.salesContentDataGridView.ReadOnly = true;
             this.salesContentDataGridView.RowHeadersVisible = false;
-            this.salesContentDataGridView.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.salesContentDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.salesContentDataGridView.Size = new System.Drawing.Size(672, 179);
-            this.salesContentDataGridView.TabIndex = 28;
             // 
             // label5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(12, 218);
+            resources.ApplyResources(this.label5, "label5");
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(337, 17);
-            this.label5.TabIndex = 27;
-            this.label5.Text = "Activities Completed During Selected Session";
             // 
             // salesDataGridView
             // 
+            resources.ApplyResources(this.salesDataGridView, "salesDataGridView");
             this.salesDataGridView.AllowUserToAddRows = false;
             this.salesDataGridView.AllowUserToDeleteRows = false;
             this.salesDataGridView.AllowUserToResizeColumns = false;
             this.salesDataGridView.AllowUserToResizeRows = false;
             this.salesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.salesDataGridView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-            this.salesDataGridView.Location = new System.Drawing.Point(195, 47);
             this.salesDataGridView.MultiSelect = false;
             this.salesDataGridView.Name = "salesDataGridView";
             this.salesDataGridView.ReadOnly = true;
             this.salesDataGridView.RowHeadersVisible = false;
-            this.salesDataGridView.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.salesDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.salesDataGridView.Size = new System.Drawing.Size(492, 154);
-            this.salesDataGridView.TabIndex = 26;
             this.salesDataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.SalesDataGridView_CellClick);
             this.salesDataGridView.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SalesDataGridView_KeyDown);
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(444, 5);
+            resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(42, 13);
-            this.label3.TabIndex = 25;
-            this.label3.Text = "Up to:";
             // 
             // newestDateTimePicker
             // 
-            this.newestDateTimePicker.Location = new System.Drawing.Point(487, 21);
+            resources.ApplyResources(this.newestDateTimePicker, "newestDateTimePicker");
             this.newestDateTimePicker.Name = "newestDateTimePicker";
-            this.newestDateTimePicker.Size = new System.Drawing.Size(200, 20);
-            this.newestDateTimePicker.TabIndex = 24;
             this.newestDateTimePicker.ValueChanged += new System.EventHandler(this.NewestDateTimePicker_ValueChanged);
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(192, 5);
+            resources.ApplyResources(this.label4, "label4");
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(86, 13);
-            this.label4.TabIndex = 23;
-            this.label4.Text = "Starting From:";
             // 
             // oldestDateTimePicker
             // 
-            this.oldestDateTimePicker.Location = new System.Drawing.Point(235, 21);
+            resources.ApplyResources(this.oldestDateTimePicker, "oldestDateTimePicker");
             this.oldestDateTimePicker.Name = "oldestDateTimePicker";
-            this.oldestDateTimePicker.Size = new System.Drawing.Size(200, 20);
-            this.oldestDateTimePicker.TabIndex = 22;
             this.oldestDateTimePicker.ValueChanged += new System.EventHandler(this.OldestDateTimePicker_ValueChanged);
             // 
             // roleLabel
             // 
-            this.roleLabel.AutoSize = true;
-            this.roleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.roleLabel.Location = new System.Drawing.Point(40, 142);
+            resources.ApplyResources(this.roleLabel, "roleLabel");
             this.roleLabel.Name = "roleLabel";
-            this.roleLabel.Size = new System.Drawing.Size(46, 17);
-            this.roleLabel.TabIndex = 21;
-            this.roleLabel.Text = "label3";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 125);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(108, 17);
-            this.label2.TabIndex = 20;
-            this.label2.Text = "Access Level:";
             // 
             // usernameLabel
             // 
-            this.usernameLabel.AutoSize = true;
-            this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.usernameLabel.Location = new System.Drawing.Point(40, 84);
+            resources.ApplyResources(this.usernameLabel, "usernameLabel");
             this.usernameLabel.Name = "usernameLabel";
-            this.usernameLabel.Size = new System.Drawing.Size(46, 17);
-            this.usernameLabel.TabIndex = 19;
-            this.usernameLabel.Text = "label2";
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 63);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 17);
-            this.label1.TabIndex = 18;
-            this.label1.Text = "Username: ";
             // 
             // UserSalesRecordsTemplateForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(703, 481);
             this.Controls.Add(this.printEntireButton);
             this.Controls.Add(this.printCurrentButton);
             this.Controls.Add(this.totalLabel);
@@ -218,9 +162,7 @@
             this.Controls.Add(this.usernameLabel);
             this.Controls.Add(this.label1);
             this.MaximizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(719, 520);
             this.Name = "UserSalesRecordsTemplateForm";
-            this.Text = "User\'s Sales Record";
             this.Load += new System.EventHandler(this.UserSalesRecordsTemplateForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.salesContentDataGridView)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.salesDataGridView)).EndInit();
--- GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/UserSessionActivitiesReportTemplateForm.Designer.cs
+++ GeneralStoreInventoryManagementSystem/GeneralStoreInventoryManagementSystem/UserSessionActivitiesReportTemplateForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserSessionActivitiesReportTemplateForm));
             this.label1 = new System.Windows.Forms.Label();
             this.usernameLabel = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -52,205 +53,127 @@
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 69);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Username: ";
             // 
             // usernameLabel
             // 
-            this.usernameLabel.AutoSize = true;
-            this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.usernameLabel.Location = new System.Drawing.Point(40, 90);
+            resources.ApplyResources(this.usernameLabel, "usernameLabel");
             this.usernameLabel.Name = "usernameLabel";
-            this.usernameLabel.Size = new System.Drawing.Size(46, 17);
-            this.usernameLabel.TabIndex = 1;
-            this.usernameLabel.Text = "label2";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 131);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(108, 17);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Access Level:";
             // 
             // roleLabel
             // 
-            this.roleLabel.AutoSize = true;
-            this.roleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.roleLabel.Location = new System.Drawing.Point(40, 148);
+            resources.ApplyResources(this.roleLabel, "roleLabel");
             this.roleLabel.Name = "roleLabel";
-            this.roleLabel.Size = new System.Drawing.Size(46, 17);
-            this.roleLabel.TabIndex = 3;
-            this.roleLabel.Text = "label3";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(444, 11);
+            resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(42, 13);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "Up to:";
             // 
             // newestDateTimePicker
             // 
-            this.newestDateTimePicker.Location = new System.Drawing.Point(487, 27);
+            resources.ApplyResources(this.newestDateTimePicker, "newestDateTimePicker");
             this.newestDateTimePicker.Name = "newestDateTimePicker";
-            this.newestDateTimePicker.Size = new System.Drawing.Size(200, 20);
-            this.newestDateTimePicker.TabIndex = 8;
             this.newestDateTimePicker.ValueChanged += new System.EventHandler(this.NewestDateTimePicker_ValueChanged);
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(192, 11);
+            resources.ApplyResources(this.label4, "label4");
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(86, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Starting From:";
             // 
             // oldestDateTimePicker
             // 
-            this.oldestDateTimePicker.Location = new System.Drawing.Point(235, 27);
+            resources.ApplyResources(this.oldestDateTimePicker, "oldestDateTimePicker");
             this.oldestDateTimePicker.Name = "oldestDateTimePicker";
-            this.oldestDateTimePicker.Size = new System.Drawing.Size(200, 20);
-            this.oldestDateTimePicker.TabIndex = 6;
             this.oldestDateTimePicker.ValueChanged += new System.EventHandler(this.OldestDateTimePicker_ValueChanged);
             // 
             // sessionsDataGridView
             // 
+            resources.ApplyResources(this.sessionsDataGridView, "sessionsDataGridView");
             this.sessionsDataGridView.AllowUserToAddRows = false;
             this.sessionsDataGridView.AllowUserToDeleteRows = false;
             this.sessionsDataGridView.AllowUserToResizeColumns = false;
             this.sessionsDataGridView.AllowUserToResizeRows = false;
             this.sessionsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.sessionsDataGridView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-            this.sessionsDataGridView.Location = new System.Drawing.Point(195, 53);
             this.sessionsDataGridView.MultiSelect = false;
             this.sessionsDataGridView.Name = "sessionsDataGridView";
             this.sessionsDataGridView.ReadOnly = true;
             this.sessionsDataGridView.RowHeadersVisible = false;
-            this.sessionsDataGridView.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.sessionsDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.sessionsDataGridView.Size = new System.Drawing.Size(492, 154);
-            this.sessionsDataGridView.TabIndex = 10;
             this.sessionsDataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.SessionsDataGridView_CellClick);
             this.sessionsDataGridView.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SessionsDataGridView_KeyDown);
             // 
             // label5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(12, 224);
+            resources.ApplyResources(this.label5, "label5");
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(337, 17);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "Activities Completed During Selected Session";
             // 
             // activitiesDataGridView
             // 
+            resources.ApplyResources(this.activitiesDataGridView, "activitiesDataGridView");
             this.activitiesDataGridView.AllowUserToAddRows = false;
             this.activitiesDataGridView.AllowUserToDeleteRows = false;
             this.activitiesDataGridView.AllowUserToResizeColumns = false;
             this.activitiesDataGridView.AllowUserToResizeRows = false;
             this.activitiesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.activitiesDataGridView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-            this.activitiesDataGridView.Location = new System.Drawing.Point(15, 244);
             this.activitiesDataGridView.MultiSelect = false;
             this.activitiesDataGridView.Name = "activitiesDataGridView";
             this.activitiesDataGridView.ReadOnly = true;
             this.activitiesDataGridView.RowHeadersVisible = false;
-            this.activitiesDataGridView.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.activitiesDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.activitiesDataGridView.Size = new System.Drawing.Size(672, 179);
-            this.activitiesDataGridView.TabIndex = 12;
             // 
             // totalLabel
             // 
-            this.totalLabel.AutoSize = true;
-            this.totalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.totalLabel.Location = new System.Drawing.Point(15, 426);
+            resources.ApplyResources(this.totalLabel, "totalLabel");
             this.totalLabel.Name = "totalLabel";
-            this.totalLabel.Size = new System.Drawing.Size(44, 13);
-            this.totalLabel.TabIndex = 13;
-            this.totalLabel.Text = "Total: ";
             // 
             // errorsLabel
             // 
-            this.errorsLabel.AutoSize = true;
-            this.errorsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.errorsLabel.Location = new System.Drawing.Point(192, 426);
+            resources.ApplyResources(this.errorsLabel, "errorsLabel");
             this.errorsLabel.Name = "errorsLabel";
-            this.errorsLabel.Size = new System.Drawing.Size(48, 13);
-            this.errorsLabel.TabIndex = 14;
-            this.errorsLabel.Text = "Errors: ";
             // 
             // salesLabel
             // 
-            this.salesLabel.AutoSize = true;
-            this.salesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.salesLabel.Location = new System.Drawing.Point(371, 426);
+            resources.ApplyResources(this.salesLabel, "salesLabel");
             this.salesLabel.Name = "salesLabel";
-            this.salesLabel.Size = new System.Drawing.Size(51, 13);
-            this.salesLabel.TabIndex = 15;
-            this.salesLabel.Text = "Sales*: ";
             this.salesLabel.MouseLeave += new System.EventHandler(this.SalesLabel_MouseLeave);
             this.salesLabel.MouseHover += new System.EventHandler(this.SalesLabel_MouseHover);
             // 
             // printCurrentButton
             // 
-            this.printCurrentButton.Location = new System.Drawing.Point(551, 446);
+            resources.ApplyResources(this.printCurrentButton, "printCurrentButton");
             this.printCurrentButton.Name = "printCurrentButton";
-            this.printCurrentButton.Size = new System.Drawing.Size(136, 23);
-            this.printCurrentButton.TabIndex = 16;
-            this.printCurrentButton.Text = "Print Current Set ";
             this.printCurrentButton.UseVisualStyleBackColor = true;
             // 
             // printEntireButton
             // 
-            this.printEntireButton.Location = new System.Drawing.Point(409, 446);
+            resources.ApplyResources(this.printEntireButton, "printEntireButton");
             this.printEntireButton.Name = "printEntireButton";
-            this.printEntireButton.Size = new System.Drawing.Size(136, 23);
-            this.printEntireButton.TabIndex = 17;
-            this.printEntireButton.Text = "Print Entire Set ";
             this.printEntireButton.UseVisualStyleBackColor = true;
             // 
             // returnsLabel
             // 
-            this.returnsLabel.AutoSize = true;
-            this.returnsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.returnsLabel.Location = new System.Drawing.Point(533, 426);
+            resources.ApplyResources(this.returnsLabel, "returnsLabel");
             this.returnsLabel.Name = "returnsLabel";
-            this.returnsLabel.Size = new System.Drawing.Size(59, 13);
-            this.returnsLabel.TabIndex = 18;
-            this.returnsLabel.Text = "Returns: ";
             // 
             // saleMessageLabel
             // 
-            this.saleMessageLabel.AutoSize = true;
+            resources.ApplyResources(this.saleMessageLabel, "saleMessageLabel");
             this.saleMessageLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.saleMessageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.saleMessageLabel.Location = new System.Drawing.Point(139, 400);
             this.saleMessageLabel.Name = "saleMessageLabel";
-            this.saleMessageLabel.Size = new System.Drawing.Size(528, 13);
-            this.saleMessageLabel.TabIndex = 19;
-            this.saleMessageLabel.Text = "The sales counter only takes into account transactions that have not resulted fro" +
-    "m a return.";
             // 
             // UserSessionActivitiesReportTemplateForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(703, 481);
             this.Controls.Add(this.saleMessageLabel);
             this.Controls.Add(this.returnsLabel);
             this.Controls.Add(this.printEntireButton);
@@ -270,9 +193,7 @@
             this.Controls.Add(this.usernameLabel);
             this.Controls.Add(this.label1);
             this.MaximizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(719, 488);
             this.Name = "UserSessionActivitiesReportTemplateForm";
-            this.Text = "User Activities Report";
             this.Load += new System.EventHandler(this.UserSessionActivitiesReportTemplateForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.sessionsDataGridView)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.activitiesDataGridView)).EndInit();
