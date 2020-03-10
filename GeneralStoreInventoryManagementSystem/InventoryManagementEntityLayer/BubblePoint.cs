a6dd457b85ef86412e93e7b35700f34b86f6ca6d
djsiclait <djidjellypas92@gmail.com>
2020-03-10 18:03:01 -0400

    Documenting new entity layer code

--- GeneralStoreInventoryManagementSystem/InventoryManagementEntityLayer/BubblePoint.cs
+++ GeneralStoreInventoryManagementSystem/InventoryManagementEntityLayer/BubblePoint.cs
@@ -10,17 +10,17 @@ namespace InventoryManagementEntityLayer
     {
         public class BubblePoint
         {
-            private String username;
-            private String logInDate;
-            private String logInTime;
-            private int seconds;
+            private String username; // Username of user requesting a session
+            private String logInDate; // Date (excludint time) a session was requested
+            private String logInTime; // Time (hour and minute) a session was requested
+            private int seconds; // Total amount of seconds the user has completed after a successful session request
 
             #region Getters and Setters
             public string Username { get => username; set => username = value; }
             public string LogInDate { get => logInDate; set => logInDate = value; }
             public string LogInTime { get => logInTime; set => logInTime = value; }
             public int Seconds { get => seconds; set => seconds = value; }
-            public decimal Minutes { get => seconds / 60; }
+            public decimal Minutes { get => seconds / 60; } // Function to convert the total seconds logged in into minutes
             #endregion
         }
     }
