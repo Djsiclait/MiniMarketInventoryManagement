6be93e4ca9f44450852f1679d5ea048158be320c
djsiclait <djidjellypas92@gmail.com>
2020-05-11 17:58:35 -0400

    Create new stored procedure to register system errors

---
+++ Database Scripts/Stored Procedure Register New Error.sql
@@ -0,0 +1,34 @@
+CREATE PROC SP_Register_New_Error
+	@code varchar(10),
+	@description varchar(300),
+	@username varchar(50)
+AS
+	BEGIN
+		
+		Insert Into Tbl_Error_Logs
+		(
+			fld_error_code,
+			fld_error_description,
+			fld_error_username,
+			fld_error_timestamp
+		)
+		Values
+		(
+			@code,
+			@description,
+			@username,
+			GETDATE()
+		)
+
+	END
+
+
+
+
+
+
+
+
+
+
+
